c8038bf72d2dba065844b1b332efe7f213bdfccc
Steen,Luuk L.E. van der <luuk.vandersteen@student.fontys.nl>
2019-04-02 10:46:42 +0200

    Add project files.

---
+++ RPG_IB2_WebApplication2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RPG_IB2_WebApplication2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
